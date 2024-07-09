3eb892ccacdbe31f36c89b572789828e24021494
Himanshu Gupta <152475687+himanshugupta672@users.noreply.github.com>
2024-07-09 19:07:55 +0530

    Time: 6 ms (11.02%), Space: 83 MB (7.35%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.java
+++ 1803-average-waiting-time/1803-average-waiting-time.java
@@ -18,7 +18,6 @@ class Solution {
             ans.add(wt);
             st=pt;
         }
-        double avg=0;
         double sum=0;
         for(int i=0;i<ans.size();i++){
             sum+= ans.get(i);
