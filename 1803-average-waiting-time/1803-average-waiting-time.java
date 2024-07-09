394212ed5b45586dce18e36cb4061dd789f1b3a1
Himanshu Gupta <152475687+himanshugupta672@users.noreply.github.com>
2024-07-09 19:05:00 +0530

    Time: 7 ms (9.39%), Space: 81.7 MB (7.35%) - LeetHub

---
+++ 1803-average-waiting-time/1803-average-waiting-time.java
@@ -0,0 +1,28 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int at =0;
+        int st =0;
+        int pt =0;
+        int wt =0;
+        for(int i=0;i<customers.length;i++){
+            at= customers[i][0];
+            if(i==0){
+                st = at;
+            }
+            if(at>st){
+                st = at;
+            }
+            pt= st+customers[i][1];
+            wt=pt-at;
+            ans.add(wt);
+            st=pt;
+        }
+        double avg=0;
+        double sum=0;
+        for(int i=0;i<ans.size();i++){
+            sum+= ans.get(i);
+        }
+        return sum/ans.size();
+    }
+}
