729b6363039445c19f03104a76e34a2f153bc0c4
Himanshu Gupta <152475687+himanshugupta672@users.noreply.github.com>
2024-07-28 00:19:58 +0530

    Time: 9 ms (59.05%), Space: 53.9 MB (81.02%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -0,0 +1,50 @@
+class Pair{
+    int first;
+    int second;
+    Pair(int first,int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int vis[][] = new int[n][m];
+        Queue<Pair> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0||j==0||i==n-1||j==m-1){
+                    if(grid[i][j]==1){
+                        q.add(new Pair(i,j));
+                        vis[i][j]=1;
+                    }
+                }
+            }
+        }
+        int dRow[] = {-1,0,1,0};
+        int dCol[] = {0,1,0,-1};
+        while(!q.isEmpty()){
+            int row = q.peek().first;
+            int col = q.peek().second;
+            q.remove();
+            for(int i=0;i<4;i++){
+                int r = row + dRow[i];
+                int c = col + dCol[i];
+                if(r>=0&&r<n&&c>=0&&c<m&&grid[r][c]==1&&vis[r][c]==0){
+                    q.add(new Pair(r,c));
+                    vis[r][c] = 1;
+                }
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0&&grid[i][j]==1){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
