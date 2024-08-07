12e903fef4d1e52de836bb4b2ed6b6bcbeb6745e
Himanshu Gupta <152475687+himanshugupta672@users.noreply.github.com>
2024-08-08 03:03:11 +0530

    Time: 37 ms (17.89%), Space: 46.2 MB (7.64%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,15 +1,45 @@
+class Pair{
+    int time;
+    int count;
+    Pair(int time,int count){
+        this.time = time;
+        this.count = count;
+    }
+}
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-         int []taskFreqMp = new int[26];
-        for(char c : tasks){
-            taskFreqMp[c- 'A']++;
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new 
+        Comparator<Pair>(){
+            public int compare(Pair p1,Pair p2){
+                return -Integer.compare(p1.count,p2.count);
+            }
+        });
+        int time=0;
+        int fq[] = new int[26];
+        for(char ch: tasks){
+            fq[ch-'A']++;
         }
-        Arrays.sort(taskFreqMp);
-        int batchCnt = taskFreqMp[25];
-        int vacantSlots = --batchCnt * n;
-        for(int indx = 0; indx < 25; indx++){
-            vacantSlots -= Math.min(taskFreqMp[indx], batchCnt); 
+        for(int i=0;i<26;i++){
+            if(fq[i]>=1){
+                pq.add(new Pair(0,fq[i]));
+            }
         }
-        return vacantSlots > 0 ? tasks.length + vacantSlots : tasks.length;
+        Queue<Pair> q = new LinkedList<>();
+        while(!pq.isEmpty() || !q.isEmpty()){
+            if(!q.isEmpty() && (time-q.peek().time)>n){
+                pq.add(q.remove());
+            }
+
+            if(!pq.isEmpty()){
+                Pair p1 = pq.remove();
+                p1.count--;
+                p1.time = time;
+                if(p1.count>0){
+                q.add(p1);
+                }
+            }
+            time++;
+        }
+        return time;
     }
 }
