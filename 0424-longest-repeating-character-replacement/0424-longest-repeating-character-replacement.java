735c07b93cc3bfd12a1cb93e4fe7870b1f70d321
Himanshu Gupta <152475687+himanshugupta672@users.noreply.github.com>
2024-09-25 03:53:33 +0530

    Time: 31 ms (19.72%), Space: 44.2 MB (29.52%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -7,20 +7,12 @@ class Solution {
         int maxFre =0;
         int maxLen = 0;
         while(r<s.length()){
-            if(hm.containsKey(s.charAt(r))){
-                hm.put(s.charAt(r),hm.get(s.charAt(r))+1);
-            }else{
-                hm.put(s.charAt(r),1);
-            }
+            hm.put(s.charAt(r),hm.getOrDefault(s.charAt(r),0)+1);
             maxFre = Math.max(maxFre,hm.get(s.charAt(r)));
-            while((r-l+1)-maxFre>k){
+            if((r-l+1)-maxFre>k){
                 hm.put(s.charAt(l),hm.get(s.charAt(l))-1);
-                for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
-                    maxFre = Math.max(maxFre,entry.getValue());
-                }
                 l++;
-            }
-            if((r-l+1)-maxFre<=k){
+            }else{
                 maxLen = Math.max(maxLen,r-l+1);
             }
             r++;
